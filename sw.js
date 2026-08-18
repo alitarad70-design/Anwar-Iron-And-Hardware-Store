82abd614160c53224890d703ba87e1102a1419ce
alitarad70-design <alitarad70@gmail.com>
2026-08-18 20:16:50 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,30 @@
+const CACHE_NAME = 'anwar-store-v2';
+
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open('anwar-store-cache').then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(['./', './index.html', './manifest.json']);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
