760dd9a70f14f18a8a7eab95a1075dbe4d87a2ae
alitarad70-design <alitarad70@gmail.com>
2026-08-18 20:47:51 +0500

    Update service worker to use new cache name

--- sw.js
+++ sw.js
@@ -1,30 +1,10 @@
-const CACHE_NAME = 'anwar-store-v2';
-
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['./', './index.html', './manifest.json']);
-    })
-  );
+ e.waitUntil(
+   caches.open('bill-store').then((cache) => cache.addAll(['index.html']))
+ );
 });
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-});
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then((response) => response || fetch(e.request))
   );
 });
